b9eb95e20d1254cb2a79e84b74b3ab1dee669acd
intlKtnx <kitsunox@protonmail.com>
2022-04-23 13:23:56 +0200

    adding 3 small nets

---
+++ src/fully_connected.py
@@ -0,0 +1,65 @@
+from base_functions import *
+import torch.nn as nn
+from datetime import datetime
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(20002, 4),
+            # nn.ReLU(),
+            # nn.Linear(4, 4),
+            nn.Softmax(dim=1),
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    home_path = "/home/marcus/Dokumente/entladung/"
+    data_path = "/home/marcus/Dokumente/entladung/modified_data"
+    pattern = 'raw_data_.h5'
+
+    """
+    arguments = sys.argv
+    logging.info(arguments)
+
+    path = arguments[1]
+    pattern = arguments[2]
+    save_dir = arguments[3]
+    """
+    
+    epochs = 10
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('Using {} device'.format(device))
+
+    test_loss, test_accuracy, train_loss, train_accuracy, confusion_matrix_raw, confusion_matrix_normalized, \
+        wrong_predictions, right_predictions, validation_accuracy, validation_loss = \
+        seed_loop(Network, device, CustomDataset(data_path, pattern), epochs, 2)
+
+    for i in confusion_matrix_raw:
+        disp = ConfusionMatrixDisplay(i, display_labels=[0, 1, 2, 3])
+        disp.plot()
+        plt.show()
+
+    metrics = pandas.DataFrame({
+        'parameters': total_params(Network().to(device)),
+
+        'test_loss': test_loss,
+        'test_accuracy': test_accuracy,
+        'train_loss': train_loss,
+        'train_accuracy': train_accuracy,
+        'confusion_matrix': confusion_matrix_raw,
+        'confusion_matrix_normalized': confusion_matrix_normalized,
+        'validation_loss': validation_loss,
+        'validation_accuracy': validation_accuracy
+    })
+    metrics.to_csv(
+        f"{home_path}_fully_connected_without_relu{datetime.now().strftime('%Y-%m-%d_%H_%M_%S')}.csv")
