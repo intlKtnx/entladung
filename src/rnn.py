0256eadb2c730021d2c11076476fe00a1155e4a4
intlKtnx <kitsunox@protonmail.com>
2022-04-28 19:44:28 +0200

    changed rnn to lstm

--- src/rnn.py
+++ src/rnn.py
@@ -9,14 +9,17 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first = True)
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
 
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        out, _ = self.rnn(x, h0)
+        # out, _ = self.rnn(x, h0)
+        # or:
+        out, _ = self.lstm(x, (h0,c0))
 
         out = out[:, -1, :]
         out = self.fc(out)
